2678a3d81de70810090d5f2043823226506674b1
Sigurd Ofstad <sigurof@gmail.com>
2024-04-01 16:56:44 +0200

    Frontend: add distribution build

--- client/browser/vite.config.ts
+++ client/browser/vite.config.ts
@@ -7,12 +7,5 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://0.0.0.0:8080/',
-        //         forward: true,
-        //         rewrite: (path) => path.replace(/^\/api/, '')
-        //     }
-        // }
     }
 })
